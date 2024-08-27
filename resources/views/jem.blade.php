90cde647b0d7f2890b139ab828ba2dbbf0843dd0
Jemmylim8 <jemlimthesec@gmail.com>
2024-08-27 16:49:13 +0800

    thisismyfirstcommit

---
+++ resources/views/jem.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+</head>
+<body>
+    <center>
+    <h3 class="text-purple-500 text-[100px]  tracking-widest ">DEMO</h3>
+  
+    <a href="{{ route('dionar1')}}"> <button> GO to Demo 2</button></a>
+    
+    </center>
+</body>
+</html>
